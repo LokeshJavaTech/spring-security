fd9a6e40b6394d84645256468bc10a61c91fd847
Lokesh Mittal <lokesh.adonis@gmail.com>
2026-06-04 13:44:08 -0400

    section3-Using InMemoryUserDetailsManager to store user credentials

--- section3/src/main/java/com/lokesh/springsecurity/config/ProjectSecurityConfig.java
+++ section3/src/main/java/com/lokesh/springsecurity/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package com.lokesh.springsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -25,5 +29,12 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails readUser = User.withUsername("lokesh").password("mittal").build();
+        UserDetails adminUser = User.withUsername("admin").password("admin").build();
+        return new InMemoryUserDetailsManager(readUser, adminUser);
     }
 }
