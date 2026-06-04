23b84e702bf98947fa61bc903b50dc70211449e0
Lokesh Mittal <lokesh.adonis@gmail.com>
2026-06-04 12:14:06 -0400

    section3-Initial Commit

---
+++ section3/src/main/java/com/lokesh/springsecurity/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.lokesh.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/myAccount")
+    public String getAccount() {
+        return "This is the account details from DB";
+    }
+}
