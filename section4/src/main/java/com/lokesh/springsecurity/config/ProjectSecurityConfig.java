35d77bd2a4eea4b54d171c9782e8229fbb110f1c
Lokesh Mittal <lokesh.adonis@gmail.com>
2026-06-05 12:19:36 -0400

    section4-Initial commit

---
+++ section4/src/main/java/com/lokesh/springsecurity/config/ProjectSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.lokesh.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+    {
+        /*
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+        */
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+                .requestMatchers("/contact", "/notices", "/error").permitAll()
+        );
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails readUser = User
+                                .withUsername("lokesh")
+                                .password("{noop}mittal")       // Password will store as plain text in memory
+                                .build();
+        // Used https://bcrypt-generator.com/ to generate bcrypt hash for password = admin
+        UserDetails adminUser = User
+                                .withUsername("admin")
+                                .password("{bcrypt}$2a$12$yswSliruV1NqSYBGYolYeOx4Kx/82duOST5wULSMALj3iplBxXmmG")   // hashed password will store in memory
+                                .build();
+        return new InMemoryUserDetailsManager(readUser, adminUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
